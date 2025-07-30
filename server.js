1d761f0fed8035f67c41a3aa7acbdd36efad24f4
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2025-07-30 11:16:36 +0530

    frontend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 8080;
-app.use(express.static('public'));
+app.use(express.static('frontend'));
 app.listen(port , function(){
     console.log('Server is Runiing on port ' + port);
 });
